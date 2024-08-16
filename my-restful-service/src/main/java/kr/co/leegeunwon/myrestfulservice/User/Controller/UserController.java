8b66101d56c9cbdf965539e4132719efdf57b036
= <=>
2024-08-16 17:03:14 +0900

    REST API Level3을 위한 HATEOAS 설정

--- my-restful-service/src/main/java/kr/co/leegeunwon/myrestfulservice/User/Controller/UserController.java
+++ my-restful-service/src/main/java/kr/co/leegeunwon/myrestfulservice/User/Controller/UserController.java
@@ -5,12 +5,17 @@ import kr.co.leegeunwon.myrestfulservice.User.bean.User;
 import kr.co.leegeunwon.myrestfulservice.User.service.IUserDaoService;
 import kr.co.leegeunwon.myrestfulservice.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -23,7 +28,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
         // 사용자가 없을 경우, 예외를 발생시킨다.
@@ -31,7 +36,13 @@ public class UserController {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+
+        // 링크를 만든다
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linTo.withRel("all-users"));    // http://localhost:8080/users -> all-users
+
+        return entityModel;
     }
 
     @PostMapping("/users")
